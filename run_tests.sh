4df5e4f8b0e80b110bea557161e50e5447fac45c
Lukas Dubsik <luke@bubaci.net>
2026-04-19 17:14:38 +0200

    refactor(globl) - 0.02: Removed some files that were no longer necessary

--- run_tests.sh
+++
@@ -1,6 +0,0 @@
-rm -rf out
-mkdir -p out
-
-javac -cp junit-platform-console-standalone-1.11.4.jar -d out $(find src -name "*.java") Homework2Test.java
-
-java -jar junit-platform-console-standalone-1.11.4.jar execute --class-path out --select-class ija.ija2025.homework2.Homework2Test
