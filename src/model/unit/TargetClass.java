65361a30617c53708eb4bdb274971eb5524911e1
Lukas Dubsik <luke@bubaci.net>
2026-05-10 13:03:58 +0200

    feature(unit) - 0.02: The tag of attack

---
+++ src/model/unit/TargetClass.java
@@ -0,0 +1,9 @@
+package model.unit;
+
+/**
+ * @brief The broad target class against which the attack is evaluated
+ */
+public enum TargetClass {
+    SOFT, ///< Infantry, crews, other soft targets
+    HARD ///< Vehicles, armored units, hard targets
+}
