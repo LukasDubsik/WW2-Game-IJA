78beda0907cdcde631f2d52e7a01894130ec2fc5
Lukas Dubsik <luke@bubaci.net>
2026-05-10 13:03:27 +0200

    feature(unit) - 0.02: Added tags for unit weapons

---
+++ src/model/unit/WeaponTag.java
@@ -0,0 +1,12 @@
+package model.unit;
+
+/**
+ * @brief Tags describing the qualitative behavior of a weapon / armament
+ */
+public enum WeaponTag {
+    MG, ///< Machine-gun type armament
+    HE, ///< High-explosive / anti-soft focused armament
+    ANTI_TANK, ///< Anti-armor focused armament
+    DIRECT_FIRE, ///< Fires directly at the target
+    INDIRECT_FIRE ///< Fires indirectly, typically artillery-like
+}
